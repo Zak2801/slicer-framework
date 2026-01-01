ebde126a0e28cae5011e03a262228a40f92286e1
Zak2801 <blackoreguy@gmail.com>
2026-01-01 19:30:23 +0200

    update checker + missing texture

--- lua/autorun/sh_addon_loader_zksf.lua
+++ lua/autorun/sh_addon_loader_zksf.lua
@@ -5,14 +5,14 @@ ZKSlicerFramework = ZKSlicerFramework or {}
 
 ZKSlicerFramework.VERSION = 1
 ZKSlicerFramework.VERSION_GITHUB = 0
-ZKSlicerFramework.VERSION_TYPE = ".WS"
+ZKSlicerFramework.VERSION_TYPE = ".GIT"
 
 function ZKSlicerFramework:GetVersion()
 	return ZKSlicerFramework.VERSION
 end
 
 function ZKSlicerFramework:CheckUpdates()
-	http.Fetch("https://raw.githubusercontent.com/Zak2801/slicer-framework/blob/main/lua/autorun/sh_addon_loader_zksf.lua", function(contents,size) 
+	http.Fetch("https://raw.githubusercontent.com/Zak2801/slicer-framework/b44c6d040a001c62a16a1361d279e7915b934ca5/lua/autorun/sh_addon_loader_zksf.lua", function(contents,size) 
 		local Entry = string.match( contents, "ZKSlicerFramework.VERSION%s=%s%d+" )
 
 		if Entry then
@@ -22,17 +22,17 @@ function ZKSlicerFramework:CheckUpdates()
 		end
 
 		if ZKSlicerFramework.VERSION_GITHUB == 0 then
-			print("[ZKSlicerFramework] latest version could not be detected, You have Version: "..ZKSlicerFramework:GetVersion())
+			print("[ZKSlicerFramework] Latest version could not be detected, You have Version: "..ZKSlicerFramework:GetVersion())
 		else
 			if  ZKSlicerFramework:GetVersion() >= ZKSlicerFramework.VERSION_GITHUB then
-				print("[ZKSlicerFramework] is up to date, Version: "..ZKSlicerFramework:GetVersion())
+				print("[ZKSlicerFramework] up to date. Version: "..ZKSlicerFramework:GetVersion())
 			else
 				print("[ZKSlicerFramework] a newer version is available! Version: "..ZKSlicerFramework.VERSION_GITHUB..", You have Version: "..ZKSlicerFramework:GetVersion())
 
 				if ZKSlicerFramework.VERSION_TYPE == ".GIT" then
-					print("[ZKSlicerFramework] get the latest version at https://github.com/Zak2801/slicer-framework")
+					print("[ZKSlicerFramework] Get the latest version at https://github.com/Zak2801/slicer-framework")
 				else
-					print("[ZKSlicerFramework] restart your game/server to get the latest version!")
+					print("[ZKSlicerFramework] Restart your game/server to get the latest version!")
 				end
 
 				if CLIENT then 
@@ -44,8 +44,6 @@ function ZKSlicerFramework:CheckUpdates()
 		end
 	end)
 end
-
-ZKSlicerFramework:CheckUpdates()
 
 function ZKSlicerFramework.LoadDirectory(path)
 	local files, folders = file.Find(path .. "/*", "LUA")
@@ -92,6 +90,7 @@ if SERVER then
 		resource.AddSingleFile("materials/vgui/hack_arrows/left.png")
 		resource.AddSingleFile("materials/vgui/hack_arrows/right.png")
 		resource.AddSingleFile("materials/vgui/frame.png")
+		resource.AddSingleFile("materials/slicerframework/physbeam_color.vmt")
 		resource.AddFile("resource/fonts/scifie.ttf")
 		resource.AddFile("resource/fonts/scifi2ku.ttf")
 		resource.AddFile("resource/fonts/scifiebi.ttf")
@@ -153,6 +152,6 @@ MsgC( Color( 255, 255, 255 ), "Version........ "..version.."\n" )
 MsgC( Color( 255, 255, 255 ), "---------------------------------- \n" )
 
 
-hook.Add( "InitPostEntity", "!!!zks_sf_checkupdates", function()
-	timer.Simple(3, function() ZKSlicerFramework:CheckUpdates() end)
+hook.Add( "InitPostEntity", "!!zks_sf_checkupdates", function()
+	timer.Simple(10, function() ZKSlicerFramework:CheckUpdates() end)
 end )
