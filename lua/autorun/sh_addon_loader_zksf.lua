b44c6d040a001c62a16a1361d279e7915b934ca5
Zak2801 <blackoreguy@gmail.com>
2026-01-01 19:22:43 +0200

    adding a version checker

--- lua/autorun/sh_addon_loader_zksf.lua
+++ lua/autorun/sh_addon_loader_zksf.lua
@@ -3,6 +3,50 @@
 ---------------------------------------------------------------
 ZKSlicerFramework = ZKSlicerFramework or {}
 
+ZKSlicerFramework.VERSION = 1
+ZKSlicerFramework.VERSION_GITHUB = 0
+ZKSlicerFramework.VERSION_TYPE = ".WS"
+
+function ZKSlicerFramework:GetVersion()
+	return ZKSlicerFramework.VERSION
+end
+
+function ZKSlicerFramework:CheckUpdates()
+	http.Fetch("https://raw.githubusercontent.com/Zak2801/slicer-framework/blob/main/lua/autorun/sh_addon_loader_zksf.lua", function(contents,size) 
+		local Entry = string.match( contents, "ZKSlicerFramework.VERSION%s=%s%d+" )
+
+		if Entry then
+			ZKSlicerFramework.VERSION_GITHUB = tonumber( string.match( Entry , "%d+" ) ) or 0
+		else
+			ZKSlicerFramework.VERSION_GITHUB = 0
+		end
+
+		if ZKSlicerFramework.VERSION_GITHUB == 0 then
+			print("[ZKSlicerFramework] latest version could not be detected, You have Version: "..ZKSlicerFramework:GetVersion())
+		else
+			if  ZKSlicerFramework:GetVersion() >= ZKSlicerFramework.VERSION_GITHUB then
+				print("[ZKSlicerFramework] is up to date, Version: "..ZKSlicerFramework:GetVersion())
+			else
+				print("[ZKSlicerFramework] a newer version is available! Version: "..ZKSlicerFramework.VERSION_GITHUB..", You have Version: "..ZKSlicerFramework:GetVersion())
+
+				if ZKSlicerFramework.VERSION_TYPE == ".GIT" then
+					print("[ZKSlicerFramework] get the latest version at https://github.com/Zak2801/slicer-framework")
+				else
+					print("[ZKSlicerFramework] restart your game/server to get the latest version!")
+				end
+
+				if CLIENT then 
+					timer.Simple(25, function() 
+						chat.AddText( Color( 255, 0, 0 ), "[ZKSlicerFramework] a newer version is available!" )
+					end)
+				end
+			end
+		end
+	end)
+end
+
+ZKSlicerFramework:CheckUpdates()
+
 function ZKSlicerFramework.LoadDirectory(path)
 	local files, folders = file.Find(path .. "/*", "LUA")
 
@@ -107,3 +151,8 @@ MsgC( Color( 180, 130, 245 ), "[Zaktak's Slicer Framework]\n" )
 MsgC( Color( 255, 255, 255 ), "Loading Files.......\n" )
 MsgC( Color( 255, 255, 255 ), "Version........ "..version.."\n" )
 MsgC( Color( 255, 255, 255 ), "---------------------------------- \n" )
+
+
+hook.Add( "InitPostEntity", "!!!zks_sf_checkupdates", function()
+	timer.Simple(3, function() ZKSlicerFramework:CheckUpdates() end)
+end )
